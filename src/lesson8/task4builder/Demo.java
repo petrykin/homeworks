f0a4cddbf85b27fc07fffa7fa27dbc197a6f7d7e
Petrykin Sergey <petrykin.sergey@gmail.com>
2017-06-01 14:00:43 +0300

    Delete Demo.java

--- src/lesson8/task4builder/Demo.java
+++
@@ -1,8 +0,0 @@
-package lesson8.task4builder;
-
-public class Demo {
-    public static void main(String[] args) {
-        Flower flower = new FlowerBuilder().name("Роза").build();
-        System.out.println(flower);
-    }
-}
