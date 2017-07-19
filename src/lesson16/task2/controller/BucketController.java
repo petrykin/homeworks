bdb11d3ebf61d027d4b5353125b66b51da50cf16
petrykin <petrykin.sergey@gmail.com>
2017-07-19 09:20:11 +0300

    add class BucketDAO and fix class BucketController #12

--- src/lesson16/task2/controller/BucketController.java
+++ src/lesson16/task2/controller/BucketController.java
@@ -10,19 +10,6 @@ import java.sql.SQLException;
 import java.util.Map;
 
 public class BucketController {
-    private final static String insertQuery = "INSERT INTO buckets (user_id, item_id, quantity) VALUES (" +
-            "(SELECT user_id FROM shop.users WHERE login = ?), " +
-            "(SELECT item_id FROM shop.items WHERE name = ?), " +
-            "?);";
-    private final static String deleteQuery = "DELETE FROM buckets WHERE user_id = " +
-            "(SELECT user_id FROM users WHERE login = ?)";
-    private final static String getQuery = "SELECT * FROM buckets INNER JOIN items " +
-            "ON buckets.item_id = items.item_id " +
-            "INNER JOIN categories " +
-            "ON items.category_id = categories.category_id " +
-            "WHERE user_id = " +
-            "(SELECT user_id FROM users WHERE login = ?);";
-
     public static boolean serializeBucket(User user, String serFileName) {
         try (FileOutputStream fos = new FileOutputStream(serFileName);
              ObjectOutputStream os = new ObjectOutputStream(fos)) {
@@ -44,46 +31,10 @@ public class BucketController {
     }
 
     public static boolean serializeBucket(User user) {
-        try (Connection connection = ConnectorDB.getConnection();
-             PreparedStatement deleteStatement = connection.prepareStatement(deleteQuery);
-             PreparedStatement insertStatement = connection.prepareStatement(insertQuery)) {
-            deleteStatement.setString(1, user.getLogin());
-            deleteStatement.executeUpdate();
-            insertStatement.setString(1, user.getLogin());
-            for (Map.Entry<Item, Integer> entry : user.getBucket().getItems().entrySet()) {
-                insertStatement.setString(2, entry.getKey().getName());
-                insertStatement.setInt(3, entry.getValue());
-                insertStatement.addBatch();
-            }
-            insertStatement.executeBatch();
-            return true;
-        } catch (SQLException e) {
-            System.err.println("Serialize bucket SQLException: " + e);
-        }
-        return false;
+        return BucketDAO.saveBucketState(user);
     }
 
     public static boolean deserializeBucket(User user) {
-        try (Connection connection = ConnectorDB.getConnection();
-             PreparedStatement getStatement = connection.prepareStatement(getQuery)) {
-            getStatement.setString(1, user.getLogin());
-            ResultSet resultSet = getStatement.executeQuery();
-            while (resultSet.next()) {
-                int quantity = resultSet.getInt("quantity");
-                Item item = new Item(
-                        resultSet.getString("items.name"),
-                        resultSet.getDouble("cost"),
-                        resultSet.getInt("rank"),
-                        Category.valueOf(resultSet.getString("categories.name"))
-                );
-                while (quantity-- > 0) {
-                    user.getBucket().addItem(item);
-                }
-            }
-            return true;
-        } catch (SQLException e) {
-            System.err.println("Deserialize bucket SQLException: " + e);
-        }
-        return false;
+        return BucketDAO.restoreBucketState(user);
     }
 }
